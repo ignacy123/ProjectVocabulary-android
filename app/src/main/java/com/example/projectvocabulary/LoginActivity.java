f83eaad6f8f6f986e8c5a833118dd4247e03057b
ignacy <ignacy.buczek@onet.pl>
2017-07-07 14:34:50 +0200

    Added manifest

--- app/src/main/java/com/example/projectvocabulary/LoginActivity.java
+++ app/src/main/java/com/example/projectvocabulary/LoginActivity.java
@@ -2,50 +2,31 @@ package com.example.projectvocabulary;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.projectvocabulary.domain.user.User;
 import com.example.projectvocabulary.network.LoginRequestDto;
 import com.example.projectvocabulary.network.ProjectVocabularyApi;
-import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,8 +41,6 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -185,27 +164,27 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     .build();
 
             ProjectVocabularyApi service = retrofit.create(ProjectVocabularyApi.class);
-                service.login(new LoginRequestDto(email, password)).enqueue(new Callback<User>() {
-                    @Override
-                    public void onResponse(Call<User> call, Response<User> response) {
-                        showProgress(false);
-                        if(response.isSuccessful()) {
-                            Log.d("TAG", response.body().getEmail());
-                            Intent intent = new Intent(LoginActivity.this, SessionActivity.class);
-                            LoginActivity.this.startActivity(intent);
-                        }else{
-                            new AlertDialog.Builder(LoginActivity.this).setMessage("BŁĄÐ").show();
-                        }
-                        System.out.println(response.body());
-                        Timber.d("Connection successful %s", response);
+            service.login(new LoginRequestDto(email, password)).enqueue(new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    showProgress(false);
+                    if (response.isSuccessful()) {
+                        Log.d("TAG", response.body().getEmail());
+                        Intent intent = new Intent(LoginActivity.this, SessionActivity.class);
+                        LoginActivity.this.startActivity(intent);
+                    } else {
+                        new AlertDialog.Builder(LoginActivity.this).setMessage("BŁĄÐ").show();
                     }
+                    System.out.println(response.body());
+                    Timber.d("Connection successful %s", response);
+                }
 
-                    @Override
-                    public void onFailure(Call<User> call, Throwable t) {
-                        showProgress(false);
-                        Timber.d("Connection failed");
-                    }
-                });
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+                    showProgress(false);
+                    Timber.d("Connection failed");
+                }
+            });
         }
 
 
@@ -362,7 +341,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
-    public void register(View view){
+
+    public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
